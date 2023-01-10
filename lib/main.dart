5f2065d662d2cfb0cc085d087143d2c43facddbf
abdullah <em.wildan.es@gmail.com>
2023-01-10 13:39:16 +0700

    remove unsed code

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'grid_page.dart';
-import 'grid_view.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,53 +11,51 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: GridPage(),
     );
   }
 }
+
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.yellow,
-        title: Text('Login'),
+        title: const Text('Login'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                margin: EdgeInsets.only(top: 30, bottom: 16),
-                padding: EdgeInsets.all(16),
-                color: Colors.lightBlue,
-                child: Text('Form Login'),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 16),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Silakan masukan username'),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 20),
-                child: TextFormField(
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Silakan masukan password'),
-                ),
-              ),
-              ElevatedButton(onPressed: () {}, child: Text('Login'))
-            ]),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Container(
+            margin: const EdgeInsets.only(top: 30, bottom: 16),
+            padding: const EdgeInsets.all(16),
+            color: Colors.lightBlue,
+            child: const Text('Form Login'),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Silakan masukan username'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Silakan masukan password'),
+            ),
+          ),
+          ElevatedButton(onPressed: () {}, child: const Text('Login'))
+        ]),
       ),
     );
   }
 }
-
